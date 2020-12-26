1654f505b48724ac83285f4d4529ba037808b08a
OlegLapukas <worldwithtime@gmail.com>
2020-12-26 23:10:57 +0200

    update

---
+++ src/com/company/Hillel/HomeWorkFiveteen/Box.java
@@ -0,0 +1,55 @@
+package com.company.Hillel.HomeWorkFiveteen;
+
+public class Box {
+
+    private int height;
+    private int width;
+    private int length;
+
+    public Box(int height, int width, int length) {
+        this.height = height;
+        this.width = width;
+        this.length = length;
+    }
+
+    public Box() {
+        //Object with default values
+        this(50, 50, 50);
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public double volume() {
+        return (double) (this.height * this.length * this.width) / 1e6;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Коробка имеет размеры в см - %n" +
+                "высота: %d%n" +
+                "ширина: %d%n" +
+                "длинна: %d%n", height, width, length);
+    }
+}
---
+++ src/com/company/Hillel/HomeWorkFiveteen/TestBox.java
@@ -0,0 +1,32 @@
+package com.company.Hillel.HomeWorkFiveteen;
+
+public class TestBox {
+
+    public static void main(String[] args) {
+        String line = "Объём коробки равен = %.4f м³.%n";
+
+        Box smallBox = new Box(10, 10, 7);
+        System.out.printf(line, smallBox.volume());
+        System.out.println(smallBox);
+
+        Box defaultBox = new Box();
+        System.out.printf(line, defaultBox.volume());
+        System.out.println(defaultBox);
+
+        Box bigBox = new Box(150, 200, 275);
+        System.out.printf(line, bigBox.volume());
+        System.out.println(bigBox);
+
+        Box unusualBox = new Box();
+        unusualBox.setHeight(17);
+        unusualBox.setLength(7);
+        unusualBox.setWidth(23);
+        System.out.printf(line, unusualBox.volume());
+        System.out.println(unusualBox);
+
+        Box box = new Box(5,5,5);
+        System.out.printf(line, box.volume());
+        System.out.println(box);
+
+    }
+}
