112c38421c1dd43701fc8b4006e075816e0ac47b
OlegLapukas <worldwithtime@gmail.com>
2020-12-26 22:24:08 +0200

    Update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-	    s
+        System.out.println("TestGitHub");
+        // write your code here
     }
 }
