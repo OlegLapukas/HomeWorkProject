4190c8f0b55de14a4e27cc2090e33b58ad57d235
OlegLapukas <worldwithtime@gmail.com>
2020-12-26 22:31:00 +0200

    add class

---
+++ src/com/company/HomeWorkEight.java
@@ -0,0 +1,88 @@
+package com.company;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class HomeWorkEight {
+
+    public static void main(String[] args) {
+
+        int size;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Введите размер массива в диапазоне от 1 до 100: ");
+            size = scanner.nextInt();
+
+            if (size > 100 || size < 1) {
+                System.out.println("Размер вне диапазона! Запустите снова и повторите ввод!");
+                return;
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Проверьте вводимые данные и запустите снова!");
+            return;
+        }
+
+        double[] array = initialize(size);
+        //System.out.println(Arrays.toString(array));
+
+        double maxValue = max(array);
+        System.out.printf("Максимальный элемент массива: %.2f%n", maxValue);
+
+        double minValue = min(array);
+        System.out.printf("Минимальный элемент массива: %.2f%n", minValue);
+
+        double averageArithmetic = average(array);
+        System.out.printf("Среднее арифметическое элементов массива: %.2f%n", averageArithmetic);
+
+    }
+
+    static double[] initialize(int size) {
+
+        double[] array = new double[size];
+
+        double value;
+
+        for (int i = 0; i < array.length; i++) {
+            value = (Math.random() - 0.5) * 10000;
+            array[i] = (double) Math.round(value) / 100;
+        }
+
+        return array;
+    }
+
+    static double max(double[] array) {
+        double max = array[0];
+
+        for (double v : array) {
+            if (v > max) {
+                max = v;
+            }
+        }
+
+        return max;
+    }
+
+    static double min(double[] array) {
+        double min = array[0];
+
+        for (double v : array) {
+            if (v < min) {
+                min = v;
+            }
+        }
+
+        return min;
+    }
+
+    static double average(double[] array) {
+        double d = 0;
+
+        for (double v : array) {
+            d += v;
+        }
+
+        d /= array.length;
+
+        return d;
+    }
+}
---
+++ src/com/company/HomeWorkThird.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class HomeWorkThird {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Введите второе число: ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Введите третье число: ");
+        double c = scanner.nextDouble();
+
+        double oneExample = a + b * c / 2;
+        double secondExample = (a * a + b * b) / 2;
+        double thirdExample = (a + b) / 12 * c % 4 + b;
+        double fourExample = (a - b * c) / (a + b) % c;
+
+        System.out.printf("%n%-21s %-30s равен: %f",
+                "Результат выражения:", "\"a + b * c / 2\"", oneExample);
+
+        System.out.printf("%n%-21s %-30s равен: %f",
+                "Результат выражения:", "\"(a * a + b * b) / 2\"", secondExample);
+
+        System.out.printf("%n%-21s %-30s равен: %f",
+                "Результат выражения:", "\"(a + b) / 12 * c % 4 + b\"", thirdExample);
+
+        System.out.printf("%n%-21s %-30s равен: %f",
+                "Результат выражения:", "\"(a - b * c) / (a + b) % c\"", fourExample);
+
+        scanner.close();
+    }
+}
