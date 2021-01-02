0eac94679c25578d9e23c46e7c07f81d7d8c6414
OlegLapukas <worldwithtime@gmail.com>
2021-01-02 21:33:48 +0200

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("TestGitHub");
-        // write your code here
+
     }
 }
