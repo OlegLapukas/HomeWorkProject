7d04280c634cdefc8e72d85b4d34b4a57a7d2097
OlegLapukas <worldwithtime@gmail.com>
2021-06-16 20:25:26 +0300

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,5 @@
 package com.company;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
